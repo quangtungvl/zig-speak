b72df630eeada4b895100cc6c7cc23551a50d276
Tung Vo <quangtungvl@gmail.com>
2024-07-16 23:53:06 +0700

    v0.6

--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const saytmp = @import("temp.zig").saytmp;
+const saytmp = @import("temp.zig");
 
 pub fn say(what: []const u8) void {
     std.debug.print("{s}\n", .{what});
