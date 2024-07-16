0a4670eee26448266472ff23c8210e0834cb8cdb
Tung Vo <quangtungvl@gmail.com>
2024-07-16 23:09:36 +0700

    v0.4

--- build.zig
+++ build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("speak", .{ .root_source_file = b.path("src/lib.zig") });
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    _ = b.addModule("speak", .{ .target = target, .optimize = optimize, .root_source_file = b.path("src/lib.zig") });
 }
